cf3d6883f44728d4fecdc33b0fe483c58fd28d97
Pablo Torres <pablotorreeifel@gmail.com>
2022-09-16 15:05:21 -0300

    not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
-  { path: '', redirectTo: 'products', pathMatch: 'full' }
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
